20a89b823a129a3eb6a058abb01ab016c26ca7a9
UnityAutomation <automationmotives@gmail.com>
2025-08-15 06:39:55 -0700

    Delete unity-automation-site directory

--- unity-automation-site/src/components/ui/textarea.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export function Textarea({ className = '', ...props }) {
-  return <textarea className={`w-full rounded-xl border border-slate-300 px-3 py-2 outline-none focus:ring-2 focus:ring-slate-300 min-h-[120px] ${className}`} {...props} />
-}
